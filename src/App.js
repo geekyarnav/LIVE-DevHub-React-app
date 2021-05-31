b2be27cb83e9db501c6fc8deb954cdc7ecfdf1e2
geeky arnav <arastogi721@gmail.com>
2021-05-31 17:37:39 +0530

    buildv1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+// Components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+
+const App = () => {
+   return (
+        <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            {/* <Route component={Routes} /> */}
+          </Switch>
+        </Fragment>
+      // </Router>
+   );
+};
+
+export default App;
